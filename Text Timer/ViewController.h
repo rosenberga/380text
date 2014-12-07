e4b4cb01ece4a86d3155d726870e360f0083f200
rosenberga <rosenbea@mail.gvsu.edu>
2014-12-07 01:49:45 -0500

    Initial Commit

---
+++ Text Timer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Text Timer
+//
+//  Created by Adam Rosenberg on 12/7/14.
+//  Copyright (c) 2014 TransientTurtle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
